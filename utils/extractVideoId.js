0994f2df409cd1a646154494a27dbab22af08053
Shawn Esquivel <shawnesquivel24@gmail.com>
2023-05-22 18:16:20 -0700

    remove excess

--- utils/extractVideoId.js
+++
@@ -1,4 +0,0 @@
-export default function extractVideoId(url) {
-  const urlParams = new URLSearchParams(new URL(url).search);
-  return urlParams.get("v");
-}
--- utils/tests/testYoutubeSearch.js
+++
@@ -1,4 +0,0 @@
-import YouTubeSearch from "../youtubeSearch.js";
-
-const searchResults = YouTubeSearch("pedro pascal");
-console.log(searchResults);
