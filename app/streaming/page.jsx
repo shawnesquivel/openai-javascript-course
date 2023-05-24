d1bd2c148ddc3a8697bdb52d39d72b52201a06cd
Shawn Esquivel <shawnesquivel24@gmail.com>
2023-05-23 18:09:24 -0700

    starter template

---
+++ app/streaming/page.jsx
@@ -0,0 +1,67 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import PageHeader from "../components/PageHeader";
+import PromptBox from "../components/PromptBox";
+import ResultStreaming from "../components/ResultStreaming";
+import Title from "../components/Title";
+import TwoColumnLayout from "app/components/TwoColumnLayout";
+
+
+const Streaming = () => {
+  const [prompt, setPrompt] = useState("");
+  const [error, setError] = useState(null);
+//   add code
+
+  const processToken = (token) => {
+
+    // add code
+    return 
+  };
+
+  const handlePromptChange = (e) => {
+    setPrompt(e.target.value);
+  };
+
+  const handleSubmit = async () => {
+    try {
+        //   add code
+    } catch (err) {
+      console.error(err);
+      setError(error);
+    }
+  };
+
+  // Clean up the EventSource on component unmount
+//   add code
+  return (
+    <>
+      <Title emoji="💭" headingText="Streaming" />
+      <TwoColumnLayout
+        leftChildren={
+          <>
+            <PageHeader
+              heading="Spit a Rap."
+              boldText="Nobody likes waiting for APIs to load. Use streaming to improve the user experience of chat bots."
+              description="This tutorial uses streaming.  Head over to Module X to get started!"
+            />
+          </>
+        }
+        rightChildren={
+          <>
+            <ResultStreaming data={data} />
+            <PromptBox
+              prompt={prompt}
+              handlePromptChange={handlePromptChange}
+              handleSubmit={handleSubmit}
+              placeHolderText={"Enter your name and city"}
+              error={error}
+              pngFile="pdf"
+            />
+          </>
+        }
+      />
+    </>
+  );
+};
+
+export default Streaming;
---
+++ pages/api/streaming.js
@@ -0,0 +1,32 @@
+import { OpenAI } from "langchain/llms/openai";
+import SSE from "express-sse";
+
+const sse = new SSE();
+
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const { input } = req.body;
+
+    if (!input) {
+      throw new Error("No input");
+    }
+    // Initialize model
+
+    // create the prompt
+
+    // call frontend to backend
+    chat
+      .call(
+        `Create me a short rap about my name and city. Make it funny and punny. Name: ${input}`
+      )
+      .then(() => {
+        sse.send(null, "end");
+      });
+
+    return res.status(200).json({ result: "OK" });
+  } else if (req.method === "GET") {
+    sse.init(req, res);
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+}
